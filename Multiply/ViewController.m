99ea456c38a883b79d6931fc9aaf55fb756e2c38
McMurdo1 <matt@1423ventures.com>
2014-01-07 16:31:25 -0600

    Created a user text input, multiplies, that by 10, and displays the answer

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,7 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    
+    __weak IBOutlet UITextField *myNumber;
+    __weak IBOutlet UILabel *myMultiplier;
+    __weak IBOutlet UILabel *myAnswer;
+}
 @end
 
 @implementation ViewController
@@ -18,6 +23,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)onCalculateButtonPressed:(id)sender {
+    NSString *myNumberString = [myNumber text];
+    int i = [myNumberString intValue];
+    
+    NSString *myMultiplierString = [myMultiplier text];
+    int m = [myMultiplierString intValue];
+    int result = i * m;
+    
+    NSString *myResult = [NSString stringWithFormat:@"%d",result];
+    [myAnswer setText:myResult];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
